f212c3a6cdc9a5a76ada80d8a0e62be3990e8837
Andrea <aalave>
2025-11-19 18:05:18 -0500

    Added Accent colors in case of darkmode being used, integrated the intro animation to the game controller and for it to start in the beginning

--- RockysSuberia/Manager/GameViewController.swift
+++ RockysSuberia/Manager/GameViewController.swift
@@ -8,25 +8,49 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import SwiftUI
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var hasShownIntro = false
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        if let view = self.view as? SKView {
-            
-            let scene = CollectScene(size: view.bounds.size)
-            scene.scaleMode = .aspectFill
-            
-            view.presentScene(scene)
-            
-            view.ignoresSiblingOrder = true
-
-            view.showsFPS = true
-            view.showsNodeCount = true
+        if !hasShownIntro {
+            hasShownIntro = true
+            presentIntroAnimation()
         }
-    }
+        
+     }
+    
+    func presentIntroAnimation() {
+            let intro = AnimationView {
+                self.startGame()
+            }
+
+            let host = UIHostingController(rootView: intro)
+            host.modalPresentationStyle = .fullScreen
+            present(host, animated: false)
+     }
+    
+    // startGame() is the same as override func viewDidLoad() but instead it's a function so it doesn't override the introduction animation in the beginning
+    // - Andrea A
+    func startGame() {
+            dismiss(animated: false) { [weak self] in
+                guard let self = self else { return }
+
+                if let view = self.view as? SKView {
+                    let scene = CollectScene(size: view.bounds.size)
+                    scene.scaleMode = .aspectFill
+                    view.presentScene(scene)
+
+                    view.ignoresSiblingOrder = true
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
